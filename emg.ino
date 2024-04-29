e14604d1149ea489232e31ff5ed0213b485f517f
yuuwe <duy.yuuwe@pm.me>
2024-04-28 20:01:11 -0700

    init

---
+++ emg.ino
@@ -0,0 +1,31 @@
+int emg_pin = A0;
+int value = 0;
+
+int c_pin = 2; // calibration pin
+int r_pin = A5; // running pin
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(c_pin, OUTPUT);
+}
+
+void loop() {
+  value = analogRead(emg_pin);
+
+  if (value > 700) {
+    value = 1;
+  } else {
+    value = 0;
+  }
+
+  Serial.println(value);
+  //digitalWrite(c_pin, HIGH);
+  
+  digitalWrite(c_pin, LOW);
+  delay(1000);
+  digitalWrite(c_pin, HIGH);
+  delay(1000);
+
+
+
+}
